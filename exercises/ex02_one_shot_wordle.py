f53d559d59e28af599aa88d000fe6a317ca5d445
agrusca <agrusca@ad.unc.edu>
2023-02-02 13:17:55 -0500

    Ex02 revision 2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -12,22 +12,22 @@ GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
 while len(original_guess) != len(secret_word):
-    #if guess is not correct length 
+    # if guess is not correct length 
     new_guess: str = input(f"That was not {len(secret_word)} letters! Try again: ")
     original_guess = new_guess
 if len(original_guess) == len(secret_word):
-    #if guess is correct length 
+    # if guess is correct length 
     while current_index < len(secret_word):
         if original_guess[current_index] == secret_word[current_index]:
-            #if specific index of guess matches specific index of secret word
+            # if specific index of guess matches specific index of secret word
             resulting_emoji = resulting_emoji + GREEN_BOX
         else: 
-            #if specific index of guess does NOT match index of secret word
+            # if specific index of guess does NOT match index of secret word
             does_character_exist: bool = False 
             alternate_index: int = 0
             while does_character_exist == False and alternate_index < len(secret_word):
                 if secret_word[alternate_index] == original_guess[current_index]:
-                    #if character in secret word at certain index matches any of the characters in guess at different index 
+                    # if character in secret word at certain index matches any of the characters in guess at different index 
                     does_character_exist = True
                 alternate_index = alternate_index + 1 
             if does_character_exist == True:
@@ -35,8 +35,8 @@ if len(original_guess) == len(secret_word):
             else:
                 resulting_emoji = resulting_emoji + WHITE_BOX
         current_index = current_index + 1
-    print (resulting_emoji)
+    print(resulting_emoji)
     if original_guess == secret_word:
-        print ("Woo! You got it!")
+        print("Woo! You got it!")
     else:
-        print ("Not quite. Play again soon!")
+        print("Not quite. Play again soon!")
