f58744282081b34c77be59d55bb0076daa3dea7a
agrusca <agrusca@ad.unc.edu>
2023-03-04 14:07:33 -0500

    utils hw

---
+++ exercises/ex05/utils.py
@@ -0,0 +1,41 @@
+"""EX05 - List utility functions, continued."""
+
+__author__ = "730309157"
+
+def only_evens(int_list: list[int]) -> list[int]:
+    """Given an int_list, returns new list contanining only even elements of int_list."""
+    even_list: list[int] = list()
+    for number in int_list:
+        if number % 2 == 0:
+            even_list.append(number)
+    return even_list
+
+
+def concat(list_1: list[int], list_2: list[int]) -> list[int]:
+    """Given list_1 and list_2, returns all elements of list_1, followed by all elements of list_2."""
+    concat_list: list[int] = list()
+    for number in list_1:
+        concat_list.append(number)
+    for number in list_2:
+        concat_list.append(number)
+    return concat_list
+
+
+def sub(list_1: list[int], start_index: int, end_index: int) -> list[int]:
+    """Given list_1, start_index, and end_index, generates list which is a subset of list_1, between start_index and end_index - 1."""
+    subset_list: list[int] = list()
+    if start_index < 0:
+        for idx in range(0, end_index):
+            subset_list.append(list_1[idx])
+        return subset_list
+    if end_index > len(list_1):
+        for idx in range(start_index, len(list_1)):
+            subset_list.append(list_1[idx])
+        return subset_list
+    if len(list_1) == 0 or start_index >= len(list_1) or end_index <= 0:
+        return subset_list
+    for idx in range(start_index, end_index):
+        subset_list.append(list_1[idx])
+    return subset_list
+    
+
---
+++ exercises/ex05/utils_test.py
@@ -0,0 +1,50 @@
+"""EX05 - Unit tests for utils functions."""
+
+__author__ = "730309157"
+
+from exercises.ex05.utils import only_evens, sub, concat
+
+def test_empty() -> None:
+    """Tests empty list in only_evens."""
+    assert only_evens([]) == []
+
+
+def test_some_evens() -> None:
+    """Tests list with some even values in only_evens."""
+    assert only_evens([1, 2, 3, 4]) == [2, 4]
+
+
+def test_all_evens() -> None: 
+    """Tests list with all even values in only_evens."""
+    assert only_evens([2, 4, 6, 8]) == [2, 4, 6, 8]
+
+
+def test_empty_lists() -> None:
+    """Tests two empty lists in concat."""
+    assert concat([], []) == []
+
+
+def test_one_list_negative() -> None:
+    """Tests one list with all negative values in concat."""
+    assert concat([-3, -2, -1], [0, 1, 2, 3]) == [-3, -2, -1, 0, 1, 2, 3]
+
+
+def test_positive_lists() -> None:
+    """Tests two lists with all positive values in concat."""
+    assert concat([1, 2, 3], [4, 5, 6]) == [1, 2, 3, 4, 5, 6]
+
+
+def test_empty_list() -> None:
+    """Tests empty list in sub."""
+    assert sub([], 1, 5) == []
+
+
+def test_index_in_range() -> None:
+    """Tests start_index and end_index within range of list in sub."""
+    assert sub([1, 2, 3, 4], 0, 3) == [1, 2, 3]
+
+
+def test_large_end_index() -> None:
+    """Tests end_index out of range of list in sub."""
+    assert sub([1, 2, 3, 4], 1, 6) == [2, 3, 4]
+
